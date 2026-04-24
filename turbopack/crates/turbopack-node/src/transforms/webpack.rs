e23916bf3c4ce7eb007c3f7f91ba4a882d8c6a9f
Luke Sandberg <lukesandberg@users.noreply.github.com>
2026-04-24 12:22:45 -0700

    simplify asset ident constructors

--- turbopack/crates/turbopack-node/src/transforms/webpack.rs
+++ turbopack/crates/turbopack-node/src/transforms/webpack.rs
@@ -156,7 +156,13 @@ impl Source for WebpackLoadersProcessedAsset {
     async fn ident(&self) -> Result<Vc<AssetIdent>> {
         Ok(
             if let Some(rename_as) = self.transform.await?.rename_as.as_deref() {
-                self.source.ident().rename_as(rename_as.into())
+                self.source
+                    .ident()
+                    .owned()
+                    .await?
+                    .rename_as(rename_as)
+                    .await?
+                    .into_vc()
             } else {
                 self.source.ident()
             },
@@ -283,7 +289,7 @@ impl WebpackLoadersProcessedAsset {
             .to_resolved()
             .await?;
 
-            let resource_fs_path = self.source.ident().path().await?;
+            let resource_fs_path = self.source.ident().await?.path.clone();
             let Some(resource_path) = project_path.get_relative_path_to(&resource_fs_path) else {
                 bail!(
                     "Resource path \"{}\" needs to be on project filesystem \"{}\"",
@@ -653,10 +659,7 @@ impl EvaluateContext for WebpackLoaderContext {
                 );
 
                 if let Some(source) = *resolved.first_source().await? {
-                    if let Some(path) = self
-                        .cwd
-                        .get_relative_path_to(&*source.ident().path().await?)
-                    {
+                    if let Some(path) = self.cwd.get_relative_path_to(&source.ident().await?.path) {
                         Ok(ResponseMessage::Resolve { path })
                     } else {
                         bail!(
@@ -941,7 +944,7 @@ impl Issue for BuildDependencyIssue {
     }
 
     async fn file_path(&self) -> Result<FileSystemPath> {
-        self.source.file_path().owned().await
+        self.source.file_path().await
     }
 
     async fn description(&self) -> Result<Option<StyledString>> {
@@ -976,7 +979,7 @@ pub struct EvaluateEmittedErrorIssue {
 #[turbo_tasks::value_impl]
 impl Issue for EvaluateEmittedErrorIssue {
     async fn file_path(&self) -> Result<FileSystemPath> {
-        self.source.file_path().owned().await
+        self.source.file_path().await
     }
 
     fn stage(&self) -> IssueStage {
@@ -1025,7 +1028,7 @@ pub struct EvaluateErrorLoggingIssue {
 #[turbo_tasks::value_impl]
 impl Issue for EvaluateErrorLoggingIssue {
     async fn file_path(&self) -> Result<FileSystemPath> {
-        self.source.file_path().owned().await
+        self.source.file_path().await
     }
 
     fn stage(&self) -> IssueStage {
